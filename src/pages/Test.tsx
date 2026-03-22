abd9b7fd50c9c2fe6eb06c28bc55d9e9d77fa29d
t-timda <jung020623@g.hongik.ac.kr>
2026-03-22 20:49:19 +0900

    #6 [FEAT] 이용약관용 모달 구현

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,17 +1,23 @@
+import TermsModal from "@/components/common/TermsModal";
 import { useNavigation } from "@react-navigation/native";
+import React from "react";
 import { View } from "react-native";
 import Button from "../components/common/Button";
-import Input from "../components/common/Input";
-
+import termsData from "../mockdatas/TermsDetail.json";
 const Test = () => {
   const navigation = useNavigation<any>();
+  const term = termsData.find((t) => t.id === 1)!; // 서비스 이용약관 데이터
+  const [visible, setVisible] = React.useState(true);
 
   return (
     <View className="flex-1 items-center justify-center gap-4">
-      <Input
-        label="테스트 입력창"
-        description="부가설명"
-        placeholder="테스트 입력창"
+      <TermsModal
+        visible={visible}
+        title={term.label}
+        required={term.required}
+        content={term.content}
+        onAgree={() => {}}
+        onClose={() => setVisible(false)}
       />
       <Button label="홈 이동" onPress={() => navigation.navigate("Tabs")} />
     </View>
