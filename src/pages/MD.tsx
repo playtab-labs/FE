7d51539899d10e46e8df7539e931b22f721b86a5
김준희 <shadd3338@gmail.com>
2026-03-24 11:15:46 +0900

    #13 [FEAT] MD 페이지 기본 레이아웃 구현

---
+++ src/pages/MD.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout';
+import { View } from 'react-native';
+
+
+export default function StampTour() {
+  return (
+    <Layout title="MD" showBack scrollable showBottomBar>
+      <View style={{ height: 40 }} />
+    </Layout>
+  );
+}
