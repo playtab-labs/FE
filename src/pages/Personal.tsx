d8548dd7c5adbd1c2e1c1d17f14639fc6a20dc26
이윤서 <yyoo04@naver.com>
2026-04-06 20:51:05 +0900

    #29 [FILE] 파일 위치 변경

--- src/pages/Personal.tsx
+++ src/pages/Personal.tsx
@@ -1,9 +1,49 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from "react-native";
+import Layout from "@/components/Layout";
+import Button from "@/components/common/Button";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import type { RootStackParamList } from "@/navigation/types";
 
 export default function Personal() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
-    <View>
-      <Text>Personal 화면 입니다.</Text>
-    </View>
+    <Layout title="PERSONAL" showBack={true}>
+      <View className="flex-1 flex-col">
+        {/* 문구 */}
+        <View className="py-4 mt-2 gap-4 px-5">
+          <View className="flex-row items-center">
+            <Text className="text-h1 font-eb text-text-salmon">팔찌 연동</Text>
+            <Text className="text-h1 font-eb text-gray-black">
+              을 시작할게요
+            </Text>
+          </View>
+          <Text className="text-b3 font-sb text-dark-gray">
+            퍼스널라이징을 위해서는 팔찌를 연동해야 해요.
+          </Text>
+        </View>
+
+        {/* 로고 */}
+        <View className="items-center mt-[103px] mb-40">
+          <Image
+            source={require("@/assets/pngs/logo.png")}
+            style={{ width: 277, height: 173 }}
+            resizeMode="contain"
+          />
+        </View>
+
+        {/* 버튼 */}
+        <View className="mt-auto mb-6 items-center">
+          <Button
+            label="팔찌 연동 시작하기"
+            size="long"
+            state="active"
+            onPress={() => navigation.navigate("Tag")}
+          />
+        </View>
+      </View>
+    </Layout>
   );
 }
